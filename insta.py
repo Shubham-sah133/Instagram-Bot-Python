fe0a8fc35b3de8c0a0278900e6981cce82302b14
Shubham <shubhmk17@gmail.com>
2026-08-04 16:11:20 +0530

    add bot configure setting

--- insta.py
+++ insta.py
@@ -7,5 +7,16 @@ import json
 from datetime import datetime, timedelta
 import schedule
 
+follower_threshold = 800  
+follow_businesses = False
+maximum_follows_per_session = 5  
+min_unfollow_days = 3 
+max_unfollow_days = 7 
+
+
+client = Client()
+load_dotenv()
+USERNAME = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 
