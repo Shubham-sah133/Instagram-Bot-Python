cb534faeb081c0f74a4e97073dc2e01c64d370e4
Shubham <shubhmk17@gmail.com>
2026-08-04 16:06:03 +0530

    Add scheduling configuration

---
+++ insta.py
@@ -0,0 +1,11 @@
+from instagrapi import Client
+from dotenv import load_dotenv
+import os
+import random as r
+import time
+import json
+from datetime import datetime, timedelta
+import schedule
+
+
+
